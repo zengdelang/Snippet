b770123daf4722ba95eb47905bc1fc00d545a10c
zengdelang <1147250901@qq.com>
2018-03-15 01:43:36 +0800

    scroll view

---
+++ AbstractScrollViewLayout.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class AbstractScrollViewLayout : LayoutGroup
+{
+    public abstract void OnContentPositionChanged(ScrollView scrollView, Bounds viewBounds, Bounds contentBounds);
+}
