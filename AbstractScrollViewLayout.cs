6a1b94441533a5207de1721494c4338b6c528708
zengdelang <1147250901@qq.com>
2018-03-15 23:50:11 +0800

    update

--- AbstractScrollViewLayout.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
-public abstract class AbstractScrollViewLayout : LayoutGroup
-{
-    public abstract void OnContentPositionChanged(ScrollView scrollView, Bounds viewBounds, Bounds contentBounds);
-}
---
+++ BytesToString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public class BytesToString
+{
+    [MenuItem("Tools/ConvertBytesToString")]
+    public static void ConvertBytes()
+    {
+        var path = EditorUtility.OpenFilePanel("", "", "7z");
+        var data = File.ReadAllBytes(path);
+        var str = Convert.ToBase64String(data);
+        
+
+        int i = 0;
+        int r = 0;
+        var sb = new StringBuilder();
+
+        foreach (var c in str)
+        {
+            sb.Append(c);
+            ++i;
+            if (i == 64)
+            {
+                i = 0;
+                sb.Append("\r\n");
+
+                ++r;
+                if (r == 10)
+                {
+                    r = 0;
+                    sb.Append("\r\n");
+                }
+            }
+        }
+
+        File.WriteAllText("D:/test.txt", sb.ToString());
+    }
+}
